2597c38f77da19e5fb33eb3e2e0374d96cef5090
unknown <syeddh01@gmail.com>
2024-09-22 00:03:05 +0530

    game updated, project seems to be completed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let randomNum = parseInt(Math.random()*10 + 1);
-console.log(randomNum);
+let randomNum = parseInt(Math.random()*20 + 1);
+// console.log(randomNum);
 
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
@@ -30,9 +30,9 @@ function validateGuess(guess){
     if (isNaN(guess)) {
         alert(`Please enter a valid number`)        
     }else if(guess < 1){
-        displayMsg(`Please enter a number greater than 1`)
-    }else if(guess > 10){
-        displayMsg(`Please enter a number less than 10`)
+        displayMsg(`Please enter a number greater or equals 1`)
+    }else if(guess > 20){
+        displayMsg(`Please enter a number less than or equals 20`)
     }else{
         displayMsg('');
         prevGuess.push(guess);
@@ -89,6 +89,7 @@ function displayMsg(message) {
 function endGame(){
     userInput.value = '';
     userInput.setAttribute('disabled', '');
+    submit.setAttribute('disabled', '');
     p.classList.add('button');
     p.innerHTML = `<h2 id='newGame'>Start new game</h2>`;
     startOver.appendChild(p);
@@ -105,16 +106,19 @@ function endGame(){
 function newGame() {
     const newGame = document.querySelector('#newGame');
     newGame.addEventListener('click', function(e) {
-        randomNum = parseInt(Math.random()*10 + 1);
-        console.log(randomNum);
+        randomNum = parseInt(Math.random()*20 + 1);
+        // console.log(randomNum);
         
         prevGuess = [];
         numGuesses = 1;
         guessSlot.innerHTML = '';
         guessSlot.style = '';
         remaining.innerHTML = `${11 - numGuesses}`;
+
         userInput.removeAttribute('disabled');
         startOver.removeChild(p);
+        submit.removeAttribute('disabled');
+        
         displayMsg('');
         lowHigh.style = '';
         
